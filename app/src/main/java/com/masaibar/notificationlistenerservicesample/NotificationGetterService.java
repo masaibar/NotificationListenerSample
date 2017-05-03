1eaefe195eb8717e8b7f0fcb27f047371ea410f7
masaibar <orebatemoko@gmail.com>
2017-05-03 14:28:33 +0900

    グループ判別と抽出のロジック

--- app/src/main/java/com/masaibar/notificationlistenerservicesample/NotificationGetterService.java
+++ app/src/main/java/com/masaibar/notificationlistenerservicesample/NotificationGetterService.java
@@ -16,6 +16,7 @@ public class NotificationGetterService extends NotificationListenerService {
 
     private static final String TARGET_PACKAGE = "com.whatsapp";
     private static boolean isNotificationAccessEnabled = false;
+    private static long sPrevWhen;
 
     public static boolean isNotificationAccessEnabledInstance() {
         return isNotificationAccessEnabled;
@@ -44,31 +45,167 @@ public class NotificationGetterService extends NotificationListenerService {
 
         for (StatusBarNotification statusBarNotification : statusBarNotifications) {
             String packageName = statusBarNotification.getPackageName();
+
             if (!TextUtils.equals(packageName, TARGET_PACKAGE)) {
-                return;
+                continue;
             }
 
             Notification notification = statusBarNotification.getNotification();
             if (notification == null) {
                 continue;
             }
-            Bundle extra = notification.extras;
-            if (extra == null) {
+            Bundle extras = notification.extras;
+            if (extras == null) {
                 continue;
             }
 
-            Log.d("!!!", String.format("============ %s ============", packageName));
+
+            //summaryチェック
+            CharSequence charSummary = extras.getCharSequence(Notification.EXTRA_SUMMARY_TEXT);
+            if (TextUtils.isEmpty(charSummary)) {
+                continue;
+            }
+
+            if (TextUtils.isEmpty(charSummary.toString())) {
+                continue;
+            }
+
+            //whenが新しいか
+            if (statusBarNotification.getNotification().when <= sPrevWhen) {
+                continue;
+            }
+
+            printDebug(statusBarNotification);
+            printLast(statusBarNotification);
+            sPrevWhen = notification.when;
+        }
+    }
+
+    private void printLast(StatusBarNotification sbn) {
+        Notification notification = sbn.getNotification();
+        Bundle extras = notification.extras;
+        long when = notification.when;
+
+        CharSequence[] textLines = extras.getCharSequenceArray(Notification.EXTRA_TEXT_LINES);
+        if (textLines == null || textLines.length < 1) {
+            //single pattern
+            if (isGroupTalk()) {
+
+            } else {
+                Log.d("!!!!!",
+                        String.format(
+                                "[Get from Single] (%s) title = %s, content = %s",
+                                when,
+                                extras.getCharSequence(Notification.EXTRA_TITLE),
+                                extras.getCharSequence(Notification.EXTRA_TEXT)
+                        ));
+            }
+        } else {
+            //multiple pattern
+            String content = textLines[textLines.length - 1].toString();
+            String title = extras.getCharSequence(Notification.EXTRA_TITLE).toString();
+            String user = getUser(content);
+            String group = getGroup(content);
+
+            if (TextUtils.isEmpty(group) && TextUtils.equals(title, "WhatsApp")) {
+                //個人
+                Log.d("!!!!!",
+                        String.format(
+                                "[Get from Multiple] (%s) title = %s, content = %s",
+                                when,
+                                user == null ? extras.getCharSequence(Notification.EXTRA_TITLE) : user,
+                                getText(content)
+                        ));
+            } else {
+                //グループ
+                Log.d("!!!!!",
+                        String.format(
+                                "[Get from Multiple] (%s) title = %s, content = %s",
+                                when,
+                                group == null ? title : group,
+                                user == null ? extras.getCharSequence(Notification.EXTRA_TITLE) : user + ": " +getText(content)
+                        ));
+            }
+        }
+    }
+
+    private boolean isGroupTalk() {
+        return false;
+    }
+
+    private static final String SPLITTER_USER = ": ";
+    private static final String SPLITTER_GROUP = " @ ";
+
+    private String getUser(String content) {
+        int splitterIndex = content.indexOf(SPLITTER_USER);
+        if (splitterIndex < 0) {
+            return null;
+        }
+
+        String user = content.substring(0, splitterIndex);
+        String group = getGroupFromUser(user);
+
+        return group == null ? user : user.replaceAll(group, "");
+    }
+
+    private String getGroupFromUser(String content) {
+        int splitterIndex = content.indexOf(SPLITTER_GROUP);
+        if (splitterIndex < 0) {
+            return null;
+        }
+
+        return content.substring(splitterIndex + 1, content.length());
+    }
+
+    private String getGroup(String content) {
+        int userSplitterIndex = content.indexOf(SPLITTER_USER);
+        if (userSplitterIndex < 0) {
+            return null;
+        }
+
+        String user = content.substring(0, userSplitterIndex);
+        int groupSplitterIndex = user.indexOf(SPLITTER_GROUP);
+        if (groupSplitterIndex < 0) {
+            return null;
+        }
+
+        return user.substring(groupSplitterIndex + SPLITTER_GROUP.length(), user.length());
+    }
+
+    private String getText(String content) {
+        int splitterIndex = content.indexOf(SPLITTER_USER);
+        if (splitterIndex < 0) {
+            return content;
+        }
+
+        return content.substring(splitterIndex + 1, content.length());
+    }
+
+    private void printDebug(StatusBarNotification sbn) {
+        Notification notification = sbn.getNotification();
+        String packageName = sbn.getPackageName();
+        Bundle extras = notification.extras;
+
+        Log.d("!!!", String.format("============ %s ============", packageName));
+        Log.d("!!!",
+                String.format("[%s], value = %s (%s)",
+                        packageName, "when", notification.when));
+        for (String key : extras.keySet()) {
+            Object value = extras.get(key);
+            if (value == null) {
+                continue;
+            }
             Log.d("!!!",
-                    String.format("[%s], value = %s (%s)",
-                            packageName, "when", notification.when));
-            for (String key : extra.keySet()) {
-                Object value = extra.get(key);
-                if (value == null) {
-                    continue;
+                    String.format("[%s], key = %s, value = %s (%s)",
+                            packageName, key, value.toString(), value.getClass().getName()));
+            if (TextUtils.equals(key, Notification.EXTRA_TEXT_LINES)) {
+                int index = 0;
+                for (CharSequence charSequence : extras.getCharSequenceArray(Notification.EXTRA_TEXT_LINES)) {
+                    Log.d("!!!",
+                            String.format("[TextLines], key = %s, value = %s",
+                                    index, charSequence.toString()));
+                    index++;
                 }
-                Log.d("!!!",
-                        String.format("[%s], key = %s, value = %s (%s)",
-                                packageName, key, value.toString(), value.getClass().getName()));
             }
         }
     }
